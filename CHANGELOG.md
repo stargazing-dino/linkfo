ac9ad759bed0fbf8d6a8290990f82309212d397d
Nolence <rex.magana@gmail.com>
2021-06-10 08:49:53 -0600

    Added more exports

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+## 0.0.2+2
+Exports models
+
 ## 0.0.2+1
 Exports other scrapers
 
---
+++ lib/src/models/models.dart
@@ -0,0 +1,7 @@
+export 'basic_metadata.dart';
+export 'open_graph.dart';
+export 'open_graph_audio/open_graph_audio.dart';
+export 'open_graph_image/open_graph_image.dart';
+export 'open_graph_video/open_graph_video.dart';
+export 'page_info/page_info.dart';
+export 'twitter_cards.dart';
