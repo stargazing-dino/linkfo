2599f1a5f9e30940504656e13d8f564929e16813
Nolence <rex.magana@gmail.com>
2021-06-10 21:03:02 -0600

    Added exports

--- lib/src/models/models.dart
+++ lib/src/models/models.dart
@@ -1,7 +1,9 @@
 export 'basic_metadata.dart';
+export 'open_graph/open_graph.dart';
 export 'open_graph_audio/open_graph_audio.dart';
 export 'open_graph_image/open_graph_image.dart';
 export 'open_graph_metadata.dart';
 export 'open_graph_video/open_graph_video.dart';
 export 'page_info/page_info.dart';
+export 'twitter_cards/twitter_cards.dart';
 export 'twitter_cards_metadata.dart';
